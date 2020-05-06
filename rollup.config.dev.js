50bd7f963b459bc0fdbe9b9e0434e8ef03447314
WoodNeck <saiyu915@naver.com>
2020-05-06 13:59:52 +0900

    Add prepush deploy hook

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,4 +1,6 @@
 const buildHelper = require("./config/build-helper");
+// const babel = require("rollup-plugin-babel");
+const liveServer = require("rollup-plugin-live-server");
 const glslify = require("rollup-plugin-glslify");
 
 export default buildHelper([
@@ -10,6 +12,14 @@ export default buildHelper([
     resolve: true,
     plugins: [
 			glslify(),
+      liveServer({
+        port: 8001,
+        host: "0.0.0.0",
+        root: "web",
+        file: "index.html",
+        open: true,
+        wait: 500
+      }),
     ]
   }
 ]);
